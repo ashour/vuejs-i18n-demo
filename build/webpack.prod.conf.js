72beb562e520c3d38ad4b8c697abeb38335cef74
Morteza Kalantar <m.kalantar@dsi.ir>
2019-11-24 17:06:32 +0330

    fix deprecated compress.warnings option

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -48,9 +48,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
     new UglifyJsPlugin({
       uglifyOptions: {
-        compress: {
-          warnings: false
-        }
+        warnings: false
       },
       sourceMap: config.build.productionSourceMap,
       parallel: true
